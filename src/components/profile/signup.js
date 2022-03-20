d019931a1d8d187a3893dd7000e78717defa1f10
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-03-19 23:47:40 -0400

    a4 commit

---
+++ src/components/profile/signup.js
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import * as service from "../../services/auth-service";
+
+export const Signup = () => {
+    const [newUser, setNewUser] = useState({});
+    const navigate = useNavigate();
+    const signup = () => {
+        service.signup(newUser)
+            .then(() => navigate("/profile"))
+            .catch(e => alert(e));
+    };
+    return (
+        <div>
+            <h1>Signup</h1>
+            <label>
+                Username:
+                <input onChange={(e) =>
+                    setNewUser({...newUser, username: e.target.value})}/>
+            </label>
+            <label>
+                Password:
+                <input type="password" onChange={(e) =>
+                    setNewUser({...newUser, password: e.target.value})}/>
+            </label>
+            <label>
+                Email:
+                <input type="email" onChange={(e) =>
+                    setNewUser({...newUser, email: e.target.value})}/>
+            </label>
+            <button onClick={signup}>Signup</button>
+        </div>
+    )
+};
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,28 +1,32 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-a3-node-project.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-a3-node-project.herokuapp.com/api/users";
+const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+const TUITS_API = `${BASE_URL}/api/tuits`;
+const USERS_API = `${BASE_URL}/api/users`;
+const api = axios.create({
+    withCredentials: true
+});
 
 export const findAllTuits = () =>
-    axios.get(TUITS_API)
+    api.get(TUITS_API)
         .then(response => response.data);
 
 export const findTuitById = (tid) =>
-    axios.get(`${TUITS_API}/${tid}`)
+    api.get(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
 export const findTuitByUser = (uid) =>
-    axios.get(`${USERS_API}/${uid}/tuits`)
+    api.get(`${USERS_API}/${uid}/tuits`)
         .then(response => response.data);
 
 export const createTuit = (uid, tuit) =>
-    axios.post(`${USERS_API}/${uid}/tuits`, tuit)
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
         .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
-    axios.put(`${TUITS_API}/${tid}`, tuit)
+    api.put(`${TUITS_API}/${tid}`, tuit)
         .then(response => response.data);
 
 export const deleteTuit = (tid) =>
-    axios.delete(`${TUITS_API}/${tid}`)
+    api.delete(`${TUITS_API}/${tid}`)
         .then(response => response.data);
